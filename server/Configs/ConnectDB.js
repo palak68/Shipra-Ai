2248d0ddd10f331355a1318bb93df1264937e36c
Palak Sharma <siyamukeshsharma@gmail.com>
2026-08-31 22:58:36 +0530

    database setup completed

---
+++ server/Configs/ConnectDB.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("MongoDB Connected Successfully")
+    } catch (error) {
+        console.log("DB Error" , error)
+    }
+}
+
+export default connectDB
--- server/index.js
+++ server/index.js
@@ -1,20 +1,16 @@
+import "dotenv/config";
 import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-
+import connectDB from "./Configs/ConnectDB.js";
 
 const app = express();
 
-
-
 const PORT = 8000;
 
-app.get ("/", (req, res) => {
+app.get("/", (req, res) => {
     res.json("Hello from server");
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });

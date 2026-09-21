335a82132e6558c023da44ed1ed19100ee6b4fe5
Palak Mukesh Sharma <siyamukeshsharma@gmail.com>
2026-09-21 16:15:17 +0530

    Change asset URLs to production environment
    Updated CSS and image source URLs to use production links.

--- client/public/assistant.js
+++ client/public/assistant.js
@@ -21,7 +21,7 @@
 
     link.rel = "stylesheet";
 
-    link.href = "http://localhost:5173/assistant.css";
+    link.href = "https://shipra-aiclient.onrender.com/assistant.css";
 
     document.head.appendChild(link);
 
@@ -96,7 +96,7 @@
                 <button class="shifra-mic">
 
                     <img
-                        src="http://localhost:5173/mic.svg"
+                        src="http://https://shipra-aiclient.onrender.com/mic.svg"
                         alt="mic"
                         class="shifra-mic-icon"
                     />
@@ -122,7 +122,7 @@
 
     button.innerHTML = `
         <img
-            src="http://localhost:5173/logo.png"
+            src="http://https://shipra-aiclient.onrender.com/logo.png"
             alt="logo"
         />
     `;
